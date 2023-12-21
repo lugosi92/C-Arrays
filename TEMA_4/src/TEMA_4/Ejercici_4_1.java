a2ab302f276f2edd0e444de46d7d7f0112c374e6
1DAW <1DAW@1DAW-003>
2023-12-21 12:19:04 +0100

    Tema 4

--- TEMA_4/src/TEMA_4/Ejercici_4_1.java
+++ TEMA_4/src/TEMA_4/Ejercici_4_1.java
@@ -5,14 +5,15 @@ public class Ejercici_4_1 {
 	public static void main(String[] args) {
 		// Crea tres tablas de cinco elementos: la primera de enteros, la segunda de doubles y la tercera
 		//de booleanos. Muestra las referencias en las que se encuentra cada una de las tablas.
-
-		int arrayA[]=new int [5];
-		double arrayB[]=new double[10];
-		boolean arrayC[]=new boolean[200];
+		
+		int arrayA[] = new int[5];
+		double arrayB[] = new double[5];
+		boolean arrayC[] = new boolean[5];
 		
 		System.out.println(arrayA);
 		System.out.println(arrayB);
 		System.out.println(arrayC);
 	}
 
+	
 }
--- TEMA_4/src/TEMA_4/Ejercicio_4_2.java
+++ TEMA_4/src/TEMA_4/Ejercicio_4_2.java
@@ -3,8 +3,24 @@ package TEMA_4;
 public class Ejercicio_4_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Construye una tabla de 10 elementos del tipo que quieras. Declara tres variables de tabla que
+		//referencian a la tabla creada. Comprueba, imprimiendo por pantalla, que todas las variables
+		//contienen la misma referencia.
+		
+		double tabla[]= new double[10];
+		
+		//declarar variables
+		double t1[]=tabla;
+		double t2[]=tabla;
+		double t3[]=tabla;
+	
+	    
+	    System.out.println(tabla);
+	    System.out.println(t1);
+	    System.out.println(t2);
+	    System.out.println(t3);
+	    
+	    
 	}
 
 }
